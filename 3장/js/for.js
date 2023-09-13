46a04cd838c31f95249e7217de26533f5b233a85
shindongjae2001 <129822456+shindongjae2001@users.noreply.github.com>
2023-09-13 15:10:28 +0900

    Add files via upload

---
+++ 3장/js/for.js
@@ -0,0 +1,5 @@
+const students = ["차승헌","강종우","박민재","배쉬셀","김재윤"]
+
+for(let i = 0; i < students.length; i++) {
+    document.write(`${students[i]}<br>`) 
+}
---
+++ 3장/js/foreach.js
@@ -0,0 +1,6 @@
+const students =  ["차승헌","강종우","박민재","배쉬셀","김재윤"];
+
+students.forEach(function(student){
+    document.write(`${student}`);
+});
+
