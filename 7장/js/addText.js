a196dc915e7e400f02f394b7612a0dcbd318f224
shindongjae2001 <129822456+shindongjae2001@users.noreply.github.com>
2023-10-17 13:12:22 +0900

    Add files via upload

---
+++ 7장/js/addText.js
@@ -0,0 +1,21 @@
+//버튼 요소 가져오기
+const orderBtn = document.querySelector("#order");
+//주문 정보 영역 가져오기
+const orderInfo = document.querySelector("#orderInfo"); 
+//책 제목 가져오기
+const title = document.querySelector("h2");  // (#container > h2);
+//버튼을 클릭하면...
+orderBtn.addEventListener("click", () => {
+    // 새로운 p 요소 만들기
+    let newP = document.createElement("p");
+    //텍스트 만들기 - title의 텍스트 부분만 가지고 와서 텍스트 노드로 생성
+    let textNode = document.createTextNode(title.innerText);
+
+    //텍스트 요소를 p요소에 추가
+    newP.appendChild(textNode);
+    newP.style.fontSize = " 1.5em";
+    newP.style.color="red";
+    //p요소를 orderInfo영역에 추가
+    orderInfo.appendChild(newP);
+
+}, {once : true});  //click 이벤트가 여러번 발생해도 이벤트 리스너는 한번만 실행함
---
+++ 7장/js/insert.js
@@ -0,0 +1,18 @@
+//버튼 가져오기
+const btn = document.querySelector("body > button");
+//버튼을 클릭하면
+btn.addEventListener("click",() => {
+    //p요소 만들기
+    let newP = document.createElement("p");
+    //텍스트 노드 만들기
+    let textNode = document.createTextNode("안녕하세요");
+   
+   //p의 자식으로 텍스트 추가하기(연결)
+    newP.appendChild(textNode);
+
+    //javascript를 기준으로
+    let basicNode = document.querySelectorAll("p")[2];
+
+    //앞에 추가하기
+    document.body.insertBefore(newP,basicNode);
+},{once : true});
